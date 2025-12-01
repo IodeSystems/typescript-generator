ebe044189d90014a311e4c6b0bdcd5a0c91579f9
Carl Taylor <carl@etaylor.me>
2025-12-01 02:08:56 -0800

    Prepare next development iteration: 0.0.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import nl.littlerobots.vcu.plugin.resolver.ModuleVersionCandidate
 
 group = "com.iodesystems.ts"
-version = "0.0.2-SNAPSHOT"
+version = "0.0.3-SNAPSHOT"
 description =
     "Typescript Client Generator"
 
