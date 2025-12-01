6ed8697d256db23d341882895c4e28bcc6a8731f
Carl Taylor <carl@etaylor.me>
2025-12-01 02:05:57 -0800

    Add release-related Gradle tasks and GPG signing configuration

--- buildSrc/src/main/kotlin/release-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/release-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import com.iodesystems.build.Bash.bash
+import com.iodesystems.build.Release
 import org.gradle.kotlin.dsl.kotlin
 import java.time.Duration
 
@@ -64,5 +66,88 @@ nexusPublishing {
             nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
             snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
         }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+tasks.register("versionGet") {
+    group = "release"
+    val version = version
+    doLast {
+        println(version)
+    }
+}
+tasks.register("versionSet") {
+    group = "release"
+    val overrideVersion = properties["overrideVersion"]
+    val updateMode = properties["updateMode"]!!.toString()
+    val version = properties["version"]!!.toString()
+    doLast {
+        val newVersion = Release.generateVersion(version, updateMode, overrideVersion?.toString())
+        Release.writeVersion(newVersion, version)
+    }
+}
+
+
+tasks.register("releaseStripSnapshotCommitAndTag") {
+    dependsOn(tasks.test)
+    group = "release"
+    val version = properties["version"]!!.toString()
+    doLast {
+        val status = "git status --porcelain".bash().output.trim()
+        if (status.isNotEmpty()) {
+            throw GradleException("There are changes in the working directory:\n$status")
+        }
+        val oldVersion = version
+        val newVersion = oldVersion.removeSuffix("-SNAPSHOT")
+        Release.writeVersion(newVersion, oldVersion)
+        // Validate build
+        "git add build.gradle.kts".bash()
+        "git commit -m 'Release $newVersion'".bash()
+        "git tag -a v$newVersion -m 'Release $newVersion'".bash()
+    }
+}
+tasks.register("releaseRevert") {
+    group = "release"
+    val version = properties["version"]!!.toString()
+    doLast {
+        val oldVersion = version
+        val newVersion = "$oldVersion-SNAPSHOT"
+        Release.writeVersion(newVersion, oldVersion)
+        "git reset --hard HEAD~1".bash()
+        "git tag -d v$oldVersion".bash()
+        println("Reverted to $newVersion")
+    }
+}
+tasks.register("releasePublish") {
+    group = "release"
+    dependsOn(tasks.clean, tasks.build, tasks.publish, tasks.closeAndReleaseStagingRepositories)
+    val overrideVersion = properties["overrideVersion"]?.toString()
+    val version = properties["version"]!!.toString()
+    doLast {
+        val oldVersion = version
+        val newVersion = Release.generateVersion(version, "dev", overrideVersion)
+        Release.writeVersion(newVersion, oldVersion)
+        "git add build.gradle.kts".bash()
+        "git commit -m 'Prepare next development iteration: $newVersion'".bash()
+        "git push".bash()
+        "git push --tags".bash()
+    }
+}
+tasks.register("releasePrepareNextDevelopmentIteration") {
+    group = "release"
+    val overrideVersion = properties["overrideVersion"]?.toString()
+    val version = properties["version"]!!.toString()
+    doLast {
+        val oldVersion = version
+        val newVersion = Release.generateVersion(version, "dev", overrideVersion)
+        Release.writeVersion(newVersion, oldVersion)
+        "git add build.gradle.kts".bash()
+        "git commit -m 'Prepare next development iteration: $newVersion'".bash()
+        "git push".bash()
     }
 }
